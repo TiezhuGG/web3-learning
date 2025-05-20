b7da23d03f16ed75649bd83c0405c8ac9c2d99d0
FFFFF <woshitiancai1014@gmail.com>
2025-05-21 01:22:18 +0800

    feat: add copy address and wallet disconnect feature.

--- lottery-dapp/packages/frontend/src/components/wallet/WalletButton.tsx
+++ lottery-dapp/packages/frontend/src/components/wallet/WalletButton.tsx
@@ -1,22 +1,34 @@
 import {
   Dialog,
   DialogContent,
-  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { useAccount, useBalance } from "wagmi";
+import { useAccount, useBalance, useDisconnect } from "wagmi";
 import { useState } from "react";
-import { formatAddress } from "@/lib/utils";
-import { ChevronDown } from "lucide-react";
+import { copyToClipboard, formatAddress } from "@/lib/utils";
+import { Check, ChevronDown, Copy, LogOut } from "lucide-react";
 
 export default function ConnectedWallet() {
   const { address } = useAccount();
   const { data: balanceData } = useBalance({ address });
+  const { disconnect } = useDisconnect();
   console.log("balanceData", balanceData);
 
   const [isOpen, setIsOpen] = useState(false);
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyAddress = async () => {
+    if (address) {
+      const result = await copyToClipboard(address);
+      console.log("result", result);
+      if (result) {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1500);
+      }
+    }
+  };
 
   return (
     <div>
@@ -35,7 +47,43 @@ export default function ConnectedWallet() {
             </p>
           </Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]"></DialogContent>
+
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogTitle></DialogTitle>
+          <div className="flex flex-col justify-center items-center">
+            <img
+              src={""}
+              alt="'"
+              height={80}
+              width={80}
+              className="rounded-full"
+            />
+            <div className="text-center mt-3">
+              <p className="text-lg font-bold">{formatAddress(address!)}</p>
+              <p className="text-gray-400">
+                {balanceData?.formatted} {balanceData?.symbol}
+              </p>
+            </div>
+
+            <div className="w-full flex gap-5" onClick={handleCopyAddress}>
+              <Button className="flex-1 h-auto mt-4 py-5">
+                {copied ? (
+                  <Check className="w-4 h-4" />
+                ) : (
+                  <Copy className="w-4 h-4" />
+                )}
+                {copied ? "Copied" : "Copy Address"}
+              </Button>
+              <Button
+                className="flex-1 h-auto mt-4 py-5"
+                onClick={() => disconnect()}
+              >
+                <LogOut className="w-4 h-4" />
+                Disconnect
+              </Button>
+            </div>
+          </div>
+        </DialogContent>
       </Dialog>
     </div>
   );
--- lottery-dapp/packages/frontend/src/lib/utils.ts
+++ lottery-dapp/packages/frontend/src/lib/utils.ts
@@ -8,3 +8,13 @@ export function cn(...inputs: ClassValue[]) {
 export function formatAddress(address: string) {
   return `${address.slice(0, 6)}...${address.slice(-4)}`;
 }
+
+export async function copyToClipboard(text: string) {
+  try {
+    await navigator.clipboard.writeText(text);
+    return true;
+  } catch (error) {
+    console.error("Failed to copy text: ", error);
+    return false;
+  }
+}
