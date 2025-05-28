bc38298bce1a1cfb49a600a930b5ca6c32c6338d
FFFFF <woshitiancai1014@gmail.com>
2025-05-28 11:48:51 +0800

    fix: NFT's tokenId=0 does not rendered.

--- nft-marketplace/packages/nextjs/src/hooks/useFetchNFTMetadata.ts
+++ nft-marketplace/packages/nextjs/src/hooks/useFetchNFTMetadata.ts
@@ -108,16 +108,15 @@ export function useFetchNFTMetadata() {
   // 过滤已上架的tokenId
   const filterListedTokenIds = async () => {
     // 确保tokenCounter是最新的
-    const {data: newestTokenCounter} = await refetchTokenCounter();
+    const { data: newestTokenCounter } = await refetchTokenCounter();
     const listingStatus = await Promise.all(
-      Array.from({ length: Number(newestTokenCounter)}, (_, i) =>
+      Array.from({ length: Number(newestTokenCounter) }, (_, i) =>
         getListItem(BigInt(i))
       )
     );
 
     const listedTokenIds = listingStatus
-      .map((listingItem, i) => (listingItem.price > 0n ? BigInt(i) : null))
-      .filter(Boolean) as bigint[];
+      .map((listingItem, i) => (listingItem.price > 0n ? BigInt(i) : null)) as bigint[]
     return listedTokenIds;
   };
 
